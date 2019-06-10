05f8cfaad6be2ba5042b013114f9f23459fe5684
tranminhnhan102 <50911166+tranminhnhan102@users.noreply.github.com>
2019-06-10 18:17:31 +0700

    update TC1

--- Selenium_Level2/src/com/logigear/test/ta_dashboard/pom/HomePage.java
+++ Selenium_Level2/src/com/logigear/test/ta_dashboard/pom/HomePage.java
@@ -1,12 +1,14 @@
 package com.logigear.test.ta_dashboard.pom;
 
+import org.apache.commons.logging.Log;
+
 import com.logigear.testfw.common.Common;
 import com.logigear.testfw.element.Element;
 
 public class HomePage extends GeneralPage {
 
 	// Element
-	protected Element txtBoardByPriority;
+	protected Element menuExecutionDashboard;
 
 	public HomePage() {
 		super(HomePage.class);
@@ -15,16 +17,18 @@ public class HomePage extends GeneralPage {
 	@Override
 	public void initPageElements() {
 		super.initPageElements();
-		this.txtBoardByPriority = new Element(getLocator("txtBoardByPriority").getBy());
+		this.menuExecutionDashboard = new Element(getLocator("menuExecutionDashboard").getBy());
 	}
 
 	/**
-	 * 
+	 * @author nhan.tran
+	 * @description wait page loading.
 	 * @param timeOutInSeconds
 	 * @return
 	 */
 	public HomePage waitForLoading(int timeOutInSeconds) {
-		txtBoardByPriority.waitForDisplay(timeOutInSeconds);
+		menuExecutionDashboard.waitForDisplay(timeOutInSeconds);
+		LOG.info("Page is loaded successfully");
 		return this;
 	}
 
--- Selenium_Level2/src/com/logigear/testfw/common/BasePOM.java
+++ Selenium_Level2/src/com/logigear/testfw/common/BasePOM.java
@@ -1,9 +1,12 @@
 package com.logigear.testfw.common;
 
 import java.util.Map;
+import java.util.logging.Logger;
 
 import com.logigear.testfw.conf.Locator;
 import com.logigear.testfw.conf.LocatorLoader;
+import com.logigear.testfw.conf.LogWrapper;
+import com.logigear.testfw.driver.BaseDriver;
 
 /**
  * 
@@ -13,11 +16,13 @@ import com.logigear.testfw.conf.LocatorLoader;
 public abstract class BasePOM {
 
 	private Map<String, Locator> locators = null;
+	protected static final Logger LOG = LogWrapper.createLogger(BaseDriver.class.getName());
 
 	public BasePOM(Class<?> derivedClass) {
 		String platform = TestExecutor.getInstance().getEnvironmentConf().getPlatform().toString();
 		String target = TestExecutor.getInstance().getEnvironmentConf().getRunTarget().toString();
-		// load all locators from JSON file, based on the class-name of the derived-class
+		// load all locators from JSON file, based on the class-name of the
+		// derived-class
 		locators = LocatorLoader.getLocatorsByClassName(derivedClass, platform, target);
 		initPageElements();
 	}
