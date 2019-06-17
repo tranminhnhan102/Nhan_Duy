bb9e0406998724b603cdf1a19a1908cdc758e2f8
tranminhnhan102 <50911166+tranminhnhan102@users.noreply.github.com>
2019-06-17 17:40:42 +0700

    Nhan:Update dataObject

--- Selenium_Level2/src/com/logigear/test/ta_dashboard/data_object/BaseDataObject.java
+++ Selenium_Level2/src/com/logigear/test/ta_dashboard/data_object/BaseDataObject.java
@@ -5,32 +5,23 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import com.google.gson.Gson;
 
 public class BaseDataObject {
-	private List<Object> groups;
-	private String tcName;
-	
-	public BaseDataObject(Class<?> dataClass)
-	{
-		parseData(tcName);
-	}
-	public List<Object> getGroups() {
-		return groups;
-	}
-	public void setGroups(List<Object> groups) {
-		this.groups = groups;
-	}
-	public String getTcName() {
-		return tcName;
-	}
-	public void setTcName(String tcName) {
-		this.tcName = tcName;
+
+	String tcName ="";
+	ArrayList<Object> obj = null;
+	public BaseDataObject() {
+		// TODO Auto-generated constructor stub
+		this.obj = 
 	}
 	public BaseDataObject parseData(String dataName) {
-		String filePath = new StringBuilder().append(System.getProperty("user.dir")).append(File.separator)
-				.append("resources").append(File.separator).append("dataInput").append(File.separator)
+		String filePath = new StringBuilder()
+				.append(System.getProperty("user.dir")).append(File.separator)
+				.append("resources").append(File.separator)
+				.append("dataInput").append(File.separator)
 				.append(dataName + ".json").toString();
 
 		String jsonString = "";
--- Selenium_Level2/src/com/logigear/test/ta_dashboard/testcases/MP_Testsuite.java
+++ Selenium_Level2/src/com/logigear/test/ta_dashboard/testcases/MP_Testsuite.java
@@ -1,5 +1,26 @@
 package com.logigear.test.ta_dashboard.testcases;
 
-public class MP_Testsuite {
+import static org.testng.Assert.assertEquals;
 
+import org.testng.annotations.Test;
+
+import com.logigear.test.ta_dashboard.pom.HomePage;
+import com.logigear.test.ta_dashboard.pom.PageDialog;
+import com.logigear.testfw.common.BaseTest;
+
+public class MP_Testsuite extends BaseTest{
+	
+	/**
+	 * @author nhan.tran
+	 * */
+	@Test
+	public void DA_MP_TC011()
+	{
+		HomePage homePage = precondition();
+		PageDialog pageDialog = homePage.selectAddPage();
+		boolean actualResult = pageDialog.isOpenAddPageAgains();
+		assertEquals(actualResult, false,"the second dialog is opened");
+	}
+	
+	
 }
